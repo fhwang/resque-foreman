b99e23e0eff88e5e2fd79bc6ce2b0b33d32a3627
Francis Hwang <francis.hwang@profitably.com>
2011-06-19 16:29:33 -0400

    hooks for after_ perform calls

---
+++ lib/resque-foreman.rb
@@ -0,0 +1,1 @@
+require 'resque-foreman/job'
---
+++ lib/resque-foreman/job.rb
@@ -0,0 +1,36 @@
+module Resque
+  module Plugins
+    module Foreman
+      module Job
+        def self.included(micro_job_class)
+          micro_job_class.extend(ClassMethods)
+          micro_job_class.cattr_accessor :foreman_class
+        end
+        
+        module ClassMethods
+          def after_batch_notify_foreman(batch_id, foreman_id, *other_args)
+            foreman = @foreman_class.new(foreman_id)
+            after_method = "after_" +
+              self.name.gsub(/#{macro_job_name}/, '').underscore + '_batch'
+            foreman.send(after_method) if foreman.respond_to?(after_method)
+          end
+    
+          def after_perform_notify_foreman(foreman_id, *other_args)
+            foreman = @foreman_class.new(foreman_id)
+            after_method = "after_" +
+              self.name.gsub(/#{macro_job_name}/, '').underscore
+            foreman.send(after_method) if foreman.respond_to?(after_method)
+          end
+          
+          def foreman(foreman_class)
+            @foreman_class = foreman_class
+          end
+          
+          def macro_job_name
+            @foreman_class.name.split(/::/)[-2]
+          end
+        end
+      end
+    end
+  end
+end
